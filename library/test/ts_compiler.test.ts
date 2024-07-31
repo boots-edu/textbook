568645f7580c0a77a4a5365424d4b1bfff43a6d8
Austin Cory Bart <acbart@vt.edu>
2024-07-31 10:03:52 -0400

    Add failing interface test

--- library/test/ts_compiler.test.ts
+++ library/test/ts_compiler.test.ts
@@ -1,7 +1,7 @@
 import {compile} from '../src/ts_compiler';
 
 describe('ts_compiler', () => {
-    it('should compile typescript code', async () => {
+    it('should compile basic typescript code', async () => {
         const code = `
         export const add = (a: number, b: number) => a + b;
         console.log(add(1, 2));
@@ -11,5 +11,21 @@ describe('ts_compiler', () => {
         expect(result.code).toContain(`console.log(add(1, 2));`);
         expect(result.diagnostics).toEqual([]);
         expect(Array.from(result.locals.keys())).toEqual(['add']);
+    });
+
+    it('interfaces should not be returned as locals', async () => {
+        const code = `
+        export class Dog {}
+        let ada: Dog = {};
+        function bark(dog: Dog) {}
+        export interface Ghost {
+            foo: string;
+        }
+        export type Phantom = Ghost;
+        `;
+        const result = await compile(code);
+        expect(Array.from(result.locals.keys())).toEqual(expect.arrayContaining(['Dog', 'ada', 'bark']));
+        expect(Array.from(result.locals.keys())).not.toContain('Ghost');
+        expect(Array.from(result.locals.keys())).not.toContain('Phantom');
     });
 });
